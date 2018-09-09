1ad17fb6fe37a8f310c6126cda917aac613e2f54
pratik1998 <pratikkayastha98@gmail.com>
2018-09-09 23:59:27 +0530

    Donation Example uses if and pipe concepts of AngularJS

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,13 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
 
   name="New User";
+  amount:number = 0;
+  alertMessage = true;
   constructor() {
+
   }
 
   changeUserName(userName)
   {
     this.name = userName;
+    this.alertMessage = false;
     console.log(this.name);
+  }
+
+  displayDonationAmount(value:number)
+  {
+    this.amount = parseInt(this.amount)+parseInt(value);
   }
   ngOnInit() {
   }
